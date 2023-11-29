ce36aa587824a3c82488c56d122c98edd35e62f0
CalaW <maker_cc@foxmail.com>
2023-11-29 13:49:05 +0800

    make vimtex use legacy ui in vscode

--- nvim/lua/plugin-config/vimtex.lua
+++ nvim/lua/plugin-config/vimtex.lua
@@ -15,22 +15,27 @@ vim.g.vimtex_env_toggle_math_map = {
     ["align"] = "\\(",
 }
 
--- if vim.g.vscode then
-vim.g.vimtex_compiler_enabled = 0 -- disable compiler interface
-vim.g.vimtex_complete_enabled = 0 -- disable vimtex completion
-vim.g.vimtex_doc_enabled = 0 --disable features reltaed to vimtex-latexdoc
-vim.g.vimtex_fold_enabled = 0 -- disable folding
-vim.g.vimtex_fold_bib_enabled = 0 --disable folding in .bib files
-vim.g.vimtex_imaps_enabled = 0 -- Disable vimtex insert mode mappings
-vim.g.vimtex_include_search_enabled = 0 -- disable search for included files
-vim.g.vimtex_indent_enabled = 0 -- disable indentation
-vim.g.vimtex_indent_bib_enabled = 0 -- disable indentation
-vim.t.vimtex_matchparen_enabled = 0 -- disable matching delimiters highlighting
-vim.g.vimtex_quickfix_enabled = 0 -- disable quickfix
-vim.g.vimtex_syntax_enabled = 0 -- disable syntax highlighting
-vim.g.vimtex_toc_enabled = 0 -- disable table of contents
-vim.g.vimtex_view_enabled = 0 -- disable pdf viewer
--- end
+if vim.g.vscode then
+    vim.g.vimtex_compiler_enabled = 0 -- disable compiler interface
+    vim.g.vimtex_complete_enabled = 0 -- disable vimtex completion
+    vim.g.vimtex_doc_enabled = 0 --disable features reltaed to vimtex-latexdoc
+    vim.g.vimtex_fold_enabled = 0 -- disable folding
+    vim.g.vimtex_fold_bib_enabled = 0 --disable folding in .bib files
+    vim.g.vimtex_imaps_enabled = 0 -- Disable vimtex insert mode mappings
+    vim.g.vimtex_include_search_enabled = 0 -- disable search for included files
+    vim.g.vimtex_indent_enabled = 0 -- disable indentation
+    vim.g.vimtex_indent_bib_enabled = 0 -- disable indentation
+    vim.t.vimtex_matchparen_enabled = 0 -- disable matching delimiters highlighting
+    vim.g.vimtex_quickfix_enabled = 0 -- disable quickfix
+    vim.g.vimtex_syntax_enabled = 0 -- disable syntax highlighting
+    vim.g.vimtex_toc_enabled = 0 -- disable table of contents
+    vim.g.vimtex_view_enabled = 0 -- disable pdf viewer
+    vim.g.vimtex_ui_method = {
+        ["confirm"]= "legacy",
+        ["input"]= "legacy",
+        ["select"]= "legacy",
+    }
+end
 
 if vim.fn.has 'mac' == 1 then
     vim.g.vimtex_view_method = 'skim'
